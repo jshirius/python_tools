1940a42e3d4f2c9650667fd6b05c92c499d9e3cd
taki mac <takizawa@liberteenz.jp>
2020-07-13 21:22:49 +0900

    ガンマ補正を入れた

--- image_processing/open_cv_sample_func.py
+++ image_processing/open_cv_sample_func.py
@@ -1,5 +1,5 @@
 def get_hsv_report(rgb_image, plot_show = False, statistics_show=False):
-        """hsv票色系の対応
+	"""hsv票色系の対応
         Args:
             rgb_image(obj): rgbイメージ画像
             plot_show(bool): hsv票色系のグラフをプロットするか
@@ -7,8 +7,7 @@ def get_hsv_report(rgb_image, plot_show = False, statistics_show=False):
 
         Returns:
            hsv票色系のパーセントタイルの値
-
-        """
+	"""
     hsv = cv2.cvtColor(img1, cv2.COLOR_BGR2HSV) # hsv票色系に変換
     h,s,v = cv2.split(hsv) # 各成分に分割
     
@@ -58,3 +57,41 @@ def get_hsv_report(rgb_image, plot_show = False, statistics_show=False):
     
     return out_dict
     
+#使い方の例
+img1 = cv2.imread('P4110830_dark.jpg')
+img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
+get_hsv_report(img1,True)
+
+
+
+def gamma_correction(image,gamma):  
+    
+    """ガンマ補正を利用して、画像を明るくしたり暗くしたりする
+
+
+    Args:
+        image(obj): イメージ画像
+        gamma(float): ガンマ値  0〜1までは、暗くする、1以上は明るくする
+        
+    Returns:
+       ガンマ補正後のイメージ画像
+
+    """
+    # 整数型で2次元配列を作成[256,1]  
+    lookup_table = np.zeros((256, 1), dtype = 'uint8')  
+    for loop in range(256):  
+        # γテーブルを作成  
+        lookup_table[loop][0] = 255 * pow(float(loop)/255, 1.0/gamma)  
+
+    # lookup Tableを用いて配列を変換 
+    image_gamma = cv2.LUT(image, lookup_table) 
+    
+    return image_gamma
+    # 変換後のファイルを保存  
+    
+
+# 画像ファイル  、ガンマ値
+img1 = cv2.imread('P4110830_dark.jpg',cv2.IMREAD_COLOR)
+image_gamma = gamma_correction(img1,1.8)  
+cv2.imwrite("file_name.jpg", image_gamma)  
+
