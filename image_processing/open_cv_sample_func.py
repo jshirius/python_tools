0bdb8fe1b633688afd4e9491716efb815e08a252
taki mac <takizawa@liberteenz.jp>
2020-07-17 08:20:19 +0900

    rgbの追加

--- image_processing/open_cv_sample_func.py
+++ image_processing/open_cv_sample_func.py
@@ -1,5 +1,9 @@
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
+
 def get_hsv_report(rgb_image, plot_show = False, statistics_show=False):
-   """hsv票色系の対応
+    """hsv票色系の対応
         Args:
             rgb_image(obj): rgbイメージ画像
             plot_show(bool): hsv票色系のグラフをプロットするか
@@ -7,7 +11,7 @@ def get_hsv_report(rgb_image, plot_show = False, statistics_show=False):
 
         Returns:
            hsv票色系のパーセントタイルの値
-   """
+    """
     hsv = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2HSV) # hsv票色系に変換
     h,s,v = cv2.split(hsv) # 各成分に分割
     
@@ -57,12 +61,6 @@ def get_hsv_report(rgb_image, plot_show = False, statistics_show=False):
     
     return out_dict
     
-#使い方の例
-img1 = cv2.imread('P4110830_dark.jpg')
-img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
-get_hsv_report(img1,True)
-
-
 
 def gamma_correction(image,gamma):  
     
@@ -90,8 +88,93 @@ def gamma_correction(image,gamma):
     # 変換後のファイルを保存  
     
 
-# 画像ファイル  、ガンマ値
-img1 = cv2.imread('P4110830_dark.jpg',cv2.IMREAD_COLOR)
-image_gamma = gamma_correction(img1,1.8)  
-cv2.imwrite("file_name.jpg", image_gamma)  
 
+def get_rgb_report(rgb_image, plot_show = False, plot_title= "rgb",  statistics_show=False):
+    """brgのヒストグラム・レポートを表示する
+        Args:
+            rgb_image(obj): rgbイメージ画像
+            plot_show(bool): ヒストグラムのグラフをプロットするか
+            statistics_show(bool):標準偏差を出力するか(処理が思い)
+
+        Returns:
+            rgb分布のパーセントタイルの値
+    """
+    print(rgb_image.shape)
+    r,g,b = cv2.split(rgb_image) # 各成分に分割
+
+    def get_percentile_list(k, datas):
+        #パーセントタイルを 5,50,95パーセントとする
+        #パーセントタイルを利用して、RGBの偏りを判定する
+        percentile = [5,50,95]
+        out_datas = {}
+        for i in percentile:
+            value = np.percentile(np.array(datas), i)
+            #out_datas.append(value)
+            s = k + "_"+str(i)
+            out_datas[s] = value
+        return out_datas
+    
+    out_dict = {}
+
+    #赤
+    data = get_percentile_list("r_per",r.ravel())
+    out_dict.update(data)
+    if(statistics_show == True):
+        out_dict['r_pstdev'] =  statistics.pstdev(r.ravel()/255)
+    
+    #緑
+    data = get_percentile_list("g_per",g.ravel())
+    out_dict.update(data)
+    if(statistics_show == True):
+        out_dict['g_pstdev'] =  statistics.pstdev(g.ravel()/255)
+    
+    #青   
+    data = get_percentile_list("b_per",b.ravel())
+    out_dict.update(data)
+    if(statistics_show == True):
+        out_dict['b_pstdev'] =  statistics.pstdev(b.ravel()/255)
+    
+    #ヒストグラムの描画
+    if(plot_show == True):
+        plt.figure(figsize=(8, 5))
+        plt.title(plot_title)
+        rgb_image = np.asarray(rgb_image.reshape(-1,3))
+        plt.hist(rgb_image, color=["red", "green", "blue"], histtype="step", bins=128)
+        plt.show()
+    
+    return out_dict
+
+def total_image_report(rgb_image):
+    """イメージの特徴レポートを出す
+        Args:
+            rgb_image(obj): rgbイメージ画像
+
+        Returns:
+            画像の特徴量
+    """
+    
+    #hsv票色系のデータ
+    data_dicts = get_hsv_report(rgb_image)
+    
+    #rgbの情報
+    data = get_rgb_report(rgb_image)
+    data_dicts.update(data)
+    
+    #ボケの数値
+    p = cv2.Laplacian(rgb_image, cv2.CV_64F).var()
+    data_dicts['blur'] = p
+    
+    return data_dicts
+
+
+
+#使い方の例
+#img1 = cv2.imread('file_name.jpg')
+#img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
+#data = get_hsv_report(img1,True)
+#print(data)
+
+img1 = cv2.imread('file_name.jpg')
+img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
+data = total_image_report(img1)
+print(data)
