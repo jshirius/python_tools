339eb821990ad96417b342d6eb354b20dae353bf
taki mac <takizawa@liberteenz.jp>
2020-07-20 22:05:25 +0900

    boxで指定された領域を切り出して表示する

--- image_processing/open_cv_sample_func.py
+++ image_processing/open_cv_sample_func.py
@@ -168,6 +168,79 @@ def total_image_report(rgb_image):
 
 
 
+def boxs_image_split_summary(src_img, boxs, src_img_show=False, rgb_show=False, hsv_show = False, save_path=""):
+    """
+    ・boxで囲まれた画像をファイルに出力する
+     ファイル名＝画像name_X_Ypot
+    ・フラグでヒストグラム、HSVを出す
+    ・サマリーのRGBの分布を書く
+    ・サマリーのHSVの分布を書く
+    →削除すべき背景を見つける
+    """
+    #サマリ用のRGB
+    rgb_image_sum = np.empty([2, 3])
+    hsv_image_sum = np.empty([2, 3])
+    rgb_image = np.asarray(img.reshape(-1,3))
+
+    for box in boxs:
+        box = [int(i) for i in box]
+
+        img_harf = img[ box[1] : box[3] , box[0]: box[2] ]  #配列の意味 [　横の範囲,  縦(高さ)の範囲]
+        
+        #画像保存
+        if(len(save_path)>0):
+            bgr_img = cv2.cvtColor(img_harf, cv2.COLOR_RGB2BGR)
+            #ファイル名
+            basename_without_ext = os.path.splitext(os.path.basename(save_path))[0]
+            dirname = os.path.dirname(save_path)
+            add_name = "x_%d_y_%d"% ( box[0],  box[1])
+            s = f'{dirname}/{basename_without_ext}_{add_name}.jpg'
+            cv2.imwrite(s, bgr_img)
+        
+        #RGBイメージの保存
+        rgb_image = np.asarray(img_harf.reshape(-1,3))
+        rgb_image_sum = np.append(rgb_image_sum, rgb_image, axis=0)
+
+         # hsv票色系に変換
+        hsv_harf = cv2.cvtColor(img_harf, cv2.COLOR_BGR2HSV)
+        hsv_harf = np.asarray(hsv_harf.reshape(-1,3))
+        hsv_image_sum = np.append(hsv_image_sum, hsv_harf, axis=0)
+        
+        #ラベルを描画する
+        if(src_img_show == True):
+            fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+            ax.imshow(img_harf)
+            
+        #RGBのヒストグラムを書く
+        if( rgb_show==True ):
+            open_cv_sample_func.get_rgb_report(img_harf, plot_show= True)
+        
+        
+        #HSVのヒストグラムを書く
+        if(hsv_show == True):
+            open_cv_sample_func.get_hsv_report(img_harf, plot_show= True)
+            
+    return rgb_image_sum, hsv_image_sum
+
+def plot_rgb_hsv_image(image_path, image_id):
+    #rgb_hsv画像をプロットする
+    img = cv2.imread(image_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    boxs,_ = get_boxs(train_df, image_id)
+    rgb_image_sum,hsv_image_sum =   boxs_image_split_summary(img, boxs, src_img_show=True, rgb_show=True, hsv_show = True, save_path = "./out_image/0a181bbb4")
+
+    
+    plt.figure(figsize=(12, 9))
+    plt.hist(rgb_image_sum, color=["red", "green", "blue"], histtype="step", bins=128)
+    plt.title("rgb_summary")
+    plt.show()    
+    
+    plt.figure(figsize=(12, 9))
+    plt.hist(hsv_image_sum,256,[0,256], color=["red", "green", "blue"], alpha=0.7, histtype="step", label=["Hue","Saturation","Value"])
+    plt.legend(bbox_to_anchor=(1, 1), loc='upper right', borderaxespad=0, fontsize=10)
+    plt.title("hsv_summary")
+    plt.show()
+
 #使い方の例
 #img1 = cv2.imread('file_name.jpg')
 #img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
@@ -178,3 +251,6 @@ def total_image_report(rgb_image):
 #img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
 #data = total_image_report(img1)
 #print(data)
+
+#plot_rgb_hsv_imageの使い方
+#plot_rgb_hsv_image("./train/0a181bbb4.jpg", "0a181bbb4")
